ce546c868fe53f8c4dacfbaeacb9b7bbaa9a3255
garellano <garellano@localhost>
2021-04-16 17:01:59 -0500

    Se agrega test para upload files el dev-cloud

--- src/test/resources/extra/regression/02/0002.sh
+++ src/test/resources/extra/regression/02/0002.sh
@@ -2,3 +2,7 @@ curl http://localhost:9999/api/upload2.json -X POST \
 -F 'files=@/Users/garellano/Desktop/peliculas/01.jpg' \
 -F 'files=@/Users/garellano/Desktop/peliculas/02.jpg'
 
+curl https://petstore.ci.ultrasist.net/api/upload.json -X POST \
+-F 'files=@/Users/garellano/Desktop/peliculas/01.jpg' \
+-F 'files=@/Users/garellano/Desktop/peliculas/02.jpg'
+
