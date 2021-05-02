5ee96142aa0aae8596c20c200a732d3b65544f42
garellano <garellano@localhost>
2021-05-02 15:15:42 -0500

    Se incorpora el método dummy (por el momento) insertaUsuarioDetalles

--- src/main/java/io/kebblar/petstore/api/rest/AccessController.java
+++ src/main/java/io/kebblar/petstore/api/rest/AccessController.java
@@ -1,10 +1,13 @@
 package io.kebblar.petstore.api.rest;
 
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.kebblar.petstore.api.model.domain.UsuarioDetalle;
 import io.kebblar.petstore.api.model.exceptions.ControllerException;
 import io.kebblar.petstore.api.model.request.CredencialesRequest;
 import io.kebblar.petstore.api.model.response.LoginResponse;
@@ -18,9 +21,15 @@ public class AccessController {
     public AccessController(AccessService accessService) {
         this.accessService = accessService;
     }
+
     @PostMapping(path = "/login.json", produces = "application/json; charset=utf-8")
     public LoginResponse login(@RequestBody CredencialesRequest cred) throws ControllerException {
         return accessService.login(cred.getUsuario(), cred.getClave());
     }
 
+    @PostMapping(path = "/usuario-detalles.json", produces = "application/json; charset=utf-8")
+    public LoginResponse insertaUsuarioDetalles(@RequestBody UsuarioDetalle dato) throws ControllerException {
+        System.out.println(dato); // Esto va a evolucionar por código real. Por el momento es sólo una prueba.
+        return new LoginResponse(dato, new Date(), "abc@aol.com", "jwt-bcdhbcxhdsb", null, null);
+    }
 }
