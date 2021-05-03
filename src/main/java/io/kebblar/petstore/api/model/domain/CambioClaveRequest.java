a6bb597700e47595690ca4275f7c1e8a8a0e5210
luz <cuahonteluz@gmail.com>
2021-05-03 10:45:16 -0700

    se suben cambios para conectar back con front

---
+++ src/main/java/io/kebblar/petstore/api/model/domain/CambioClaveRequest.java
@@ -0,0 +1,52 @@
+package io.kebblar.petstore.api.model.domain;
+
+public class CambioClaveRequest {
+	private String token;
+	private String clave;
+
+	public String getClave() {
+	return clave;
+	}
+	public void setClave(String clave) {
+	this.clave = clave;
+	}
+	public String getToken() {
+	return token;
+	}
+	public void setToken(String token) {
+	this.token = token;
+	}
+	@Override
+	public String toString() {
+	return "CambioClaveRequest [token=" + token + ", clave=" + clave + "]";
+	}
+	@Override
+	public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((clave == null) ? 0 : clave.hashCode());
+	result = prime * result + ((token == null) ? 0 : token.hashCode());
+	return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+	if (this == obj)
+	return true;
+	if (obj == null)
+	return false;
+	if (getClass() != obj.getClass())
+	return false;
+	CambioClaveRequest other = (CambioClaveRequest) obj;
+	if (clave == null) {
+	if (other.clave != null)
+	return false;
+	} else if (!clave.equals(other.clave))
+	return false;
+	if (token == null) {
+	if (other.token != null)
+	return false;
+	} else if (!token.equals(other.token))
+	return false;
+	return true;
+	}
+}
---
+++ src/main/java/io/kebblar/petstore/api/rest/CambioClaveController.java
@@ -0,0 +1,24 @@
+package io.kebblar.petstore.api.rest;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.kebblar.petstore.api.model.domain.CambioClaveRequest;
+import io.kebblar.petstore.api.model.exceptions.BusinessException;
+
+@RestController
+@RequestMapping(value = "/api")
+public class CambioClaveController {
+
+	@PostMapping(path = "/recuperar-clave.json", produces = "application/json; charset=utf-8")
+    public String recuperarClave(@RequestBody String correo) throws BusinessException {
+    return "Se envío un correo electrónico";
+    }
+   
+    @PostMapping(path = "/confirma-recuperar-clave.json", produces = "application/json; charset=utf-8")
+    public String confirmaRecuperarClave(@RequestBody CambioClaveRequest claves) throws BusinessException {
+    return "OK";
+    }
+}
