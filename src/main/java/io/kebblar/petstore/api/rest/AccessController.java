2c24b1725d2ff60234f3ee5912baa35b1b5f5384
garellano <garellano@localhost>
2021-05-05 11:14:46 -0500

    adding roles to accessController

--- src/main/java/io/kebblar/petstore/api/rest/AccessController.java
+++ src/main/java/io/kebblar/petstore/api/rest/AccessController.java
@@ -1,6 +1,8 @@
 package io.kebblar.petstore.api.rest;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.kebblar.petstore.api.model.domain.Rol;
 import io.kebblar.petstore.api.model.domain.UsuarioDetalle;
 import io.kebblar.petstore.api.model.exceptions.ControllerException;
 import io.kebblar.petstore.api.model.request.CredencialesRequest;
@@ -32,7 +35,12 @@ public class AccessController {
     public LoginResponse insertaUsuarioDetalles(@RequestBody UsuarioDetalle dato) throws ControllerException {
         System.out.println(dato); // Esto va a evolucionar por código real. Por el momento es sólo una prueba.
         String datos = UUID.randomUUID().toString();
-        return new LoginResponse(dato, new Date(), "abc@aol.com", "gus-jwt-"+datos, null, null);
+        List<Rol> roles = new ArrayList<>();
+        roles.add(new Rol(1,"r1",true));
+        roles.add(new Rol(2,"r2",true));
+        roles.add(new Rol(3,"r3",true));
+        
+        return new LoginResponse(dato, new Date(), "abc@aol.com", "gus-jwt-"+datos, roles, null);
     }
 
 }
