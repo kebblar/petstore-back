f0f758672a3e7ab2297e52e81900e47b2eed5c70
ulises <alejandro.urbin@gmail.com>
2021-05-11 09:44:12 -0500

    Ajuste de texto

--- src/main/java/io/kebblar/petstore/api/service/AccessService.java
+++ src/main/java/io/kebblar/petstore/api/service/AccessService.java
@@ -36,10 +36,11 @@ public interface AccessService {
      * éxito, retorna un objeto LoginResponse cargado con la información requerida
      * por su cliente. Si las credenciales son inválidas, dispara una excepción
      * acorde a el grado de violación de ingreso. Se requiere que el método siga las
-     * siguientes reglas: - No admite usuario ni clave vacios - Si el usuario no
-     * existe, sólo pide que intente de nuevo - Si el usuaro existe y la clave es
-     * errónea, le indicará que le quedan menos intentos - Si el usuario está
-     * bloqueado (con o sin clave correcta) le indicará que debe esperar cierto
+     * siguientes reglas: 
+     *  - No admite usuario ni clave vacios 
+     *  - Si el usuario no existe, sólo pide que intente de nuevo 
+     *  - Si el usuaro existe y la clave es errónea, le indicará que le quedan menos intentos 
+     *  - Si el usuario está bloqueado (con o sin clave correcta) le indicará que debe esperar cierto
      * tiempo
      * 
      * @param usuario
@@ -54,10 +55,10 @@ public interface AccessService {
      * éxito, retorna un objeto LoginResponse cargado con la información requerida
      * por su cliente. Si las credenciales son inválidas, dispara una excepción
      * acorde a el grado de violación de ingreso. Se requiere que el método siga las
-     * siguientes reglas: - No admite usuario ni clave vacios - Si el usuario no
-     * existe, sólo pide que intente de nuevo - Si el usuaro existe y la clave es
-     * errónea, le indicará que le quedan menos intentos - Si el usuario está
-     * bloqueado (con o sin clave correcta) le indicará que debe esperar cierto
+     * siguientes reglas: - No admite usuario ni clave vacios 
+     *  - Si el usuario no existe, sólo pide que intente de nuevo 
+     *  - Si el usuaro existe y la clave es errónea, le indicará que le quedan menos intentos 
+     *  - Si el usuario está bloqueado (con o sin clave correcta) le indicará que debe esperar cierto
      * tiempo
      * 
      * @param usuario
