cf396c06c730888c702c429f2696d673f7497d55
doalvarez <0010.diplomado.ultrasist@gmail.com>
2021-05-19 16:16:06 -0500

    Se agrega flujo procesa orden compra y factura pdf

--- src/main/java/io/kebblar/petstore/api/rest/PagosController.java
+++ src/main/java/io/kebblar/petstore/api/rest/PagosController.java
@@ -8,15 +8,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@Api("value = pagos")
-@RequestMapping(value="/api")
+//@RestController
+//@Api("value = pagos")
+//@RequestMapping(value="/api")
 public class PagosController {
 
-    @PostMapping( value = "/procesa-orden.json",
-            produces = "application/json; charset=utf-8")
-    public String procesaOrden(@RequestBody DatosOrden datosCompra) throws BusinessException{
-        return datosCompra.toString();
-    }
+//    @PostMapping( value = "/procesa-orden.json",
+//            produces = "application/json; charset=utf-8")
+//    public String procesaOrden(@RequestBody DatosOrden datosCompra) throws BusinessException{
+//        return datosCompra.toString();
+//    }
 
 }
