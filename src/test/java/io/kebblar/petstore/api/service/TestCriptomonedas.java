0bf87e85cd738523c5b481d80009f7e1d7a1029b
Fhernanda Romo Olea <fheromo@MacBook-Air-de-Fhernanda.local>
2021-06-01 18:59:18 -0500

    Solucionando problemas con el test de Criptomonedas

--- src/test/java/io/kebblar/petstore/api/service/TestCriptomonedas.java
+++ src/test/java/io/kebblar/petstore/api/service/TestCriptomonedas.java
@@ -1,28 +1,16 @@
 package io.kebblar.petstore.api.service;
 
 import io.kebblar.petstore.api.mapper.CriptoMapper;
-import io.kebblar.petstore.api.mapper.OrdenCompraMapper;
-import io.kebblar.petstore.api.mapper.UsuarioDetalleMapper;
-import io.kebblar.petstore.api.mapper.UsuarioMapper;
 import io.kebblar.petstore.api.model.domain.TransaccionBtc;
-import io.kebblar.petstore.api.model.domain.Usuario;
-import io.kebblar.petstore.api.model.domain.UsuarioDetalle;
-import io.kebblar.petstore.api.model.exceptions.BitcoinTransactionException;
-import io.kebblar.petstore.api.model.exceptions.BusinessException;
 import io.kebblar.petstore.api.model.exceptions.MapperCallException;
-import org.apache.poi.ss.formula.functions.T;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -35,48 +23,29 @@ public class TestCriptomonedas {
     private CriptoMapper criptoMapper;
 
     @Mock
-    private OrdenCompraMapper ordenCompraMapper;
-
-    @Mock
-    private UsuarioMapper usuarioMapper;
-
-    @Mock
-    private UsuarioDetalleMapper usuarioDetalleMapper;
-
-    @Mock
     private OrdenCompraService ordenCompraService;
 
     private RemoteRestCallService remote;
     private CriptoService criptoService;
-    List<TransaccionBtc> list;
 
     private TransaccionBtc t1 = new TransaccionBtc();
+    private TransaccionBtc t2 = new TransaccionBtc();
 
     @Before
     public void prepare() throws SQLException {
         criptoService = new CriptoServiceImpl(criptoMapper, remote, ordenCompraService);
 
-        list = new ArrayList<>();
-        TransaccionBtc transaccion1 = new TransaccionBtc(7,1,2,"12Vh6YLge3bYDrojdC1TpwjfS18sdeQjoN",1,false,700, new Date(), "pba test");
-        TransaccionBtc transaccion2 = new TransaccionBtc(8,5,28,"1MSzx9JGPEjXpWVDfeNKnDknTgqWeEyiLd",6,false,800, new Date(), "pba test 2");
-        list.add(transaccion1);
-        list.add(transaccion2);
-
         when(criptoMapper.getByUser(1)).thenReturn("cadenaWallet");
         when(criptoMapper.getByUser(2)).thenThrow(new SQLException());
         when(criptoMapper.insertTransaccion(t1)).thenReturn(1);
-        when(criptoMapper.insertTransaccion(Mockito.any())).thenThrow(new SQLException());
-        when(criptoMapper.getAll()).thenReturn(list);
-        when(ordenCompraMapper.insert(Mockito.any())).thenReturn(1);
-        when(usuarioMapper.getById(Mockito.any())).thenReturn(new Usuario());
-        when(usuarioDetalleMapper.getById(Mockito.any())).thenReturn(new UsuarioDetalle());
+        when(criptoMapper.insertTransaccion(t2)).thenThrow(new SQLException());
     }
 
     @Test
     public void getByUserTest() {
         try {
             assertEquals(criptoService.getByUser(1),"cadenaWallet");
-        } catch (MapperCallException e) {}
+        } catch (MapperCallException  e) {}
         try {
             String s = criptoService.getByUser(2);
         } catch (MapperCallException m) {
@@ -90,7 +59,7 @@ public class TestCriptomonedas {
             assertEquals(criptoService.insertTransaccion(t1),1);
         } catch (MapperCallException e) { }
         try {
-            int i = criptoService.insertTransaccion(new TransaccionBtc());
+            int i = criptoService.insertTransaccion(t2);
         } catch (MapperCallException b) {
             assertTrue(true);
         }
