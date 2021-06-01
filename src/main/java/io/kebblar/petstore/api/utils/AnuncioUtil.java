968b54bb57509739d6b614be6c61ffc8e7d4f00b
EdgarUM <ragde2593@gmail.com>
2021-05-31 21:43:19 -0700

    Cambio validacion

--- src/main/java/io/kebblar/petstore/api/utils/AnuncioUtil.java
+++ src/main/java/io/kebblar/petstore/api/utils/AnuncioUtil.java
@@ -169,7 +169,7 @@ public class AnuncioUtil {
 		
 		String startRow = getPageNumber >= 1 ? Integer.toString((getPageNumber-1)*getPageSize) : Integer.toString(getPageNumber);
 		String pageSize = Integer.toString(getPageSize);
-		if (filtros.getIdCategoria() != 0 && filtros.getIdCategoria() != null) {
+		if (filtros.getIdCategoria() != null && filtros.getIdCategoria() != 0 ) {
 			consultaBase.append(" AND id_categoria = ").append(filtros.getIdCategoria());
 		}
 		if (filtros.getPrecio() != null && filtros.getPrecio() != BigDecimal.ZERO) {
