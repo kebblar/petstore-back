766faec569122436d543e99ccb1a58d511e0ac36
Fher RO <34664567+fherevans@users.noreply.github.com>
2021-06-13 15:27:47 -0500

    Agregando cabecera

--- src/main/java/io/kebblar/petstore/api/service/RemoteRestCallServiceImpl.java
+++ src/main/java/io/kebblar/petstore/api/service/RemoteRestCallServiceImpl.java
@@ -1,3 +1,21 @@
+/*
+ * Licencia:    Usted  puede  utilizar  libremente  este  código
+ *              para  copiarlo, distribuirlo o modificarlo total
+ *              o  parcialmente  siempre y cuando  mantenga este
+ *              aviso y reconozca la  autoría  del  código al no
+ *              modificar los  datos  establecidos en la mención
+ *              de: "AUTOR".
+ *
+ *              ------------------------------------------------
+ * Artefacto:   RemoteRestCallServiceImpl.java
+ * Tipo:        clase
+ * AUTOR:       Gustavo A. Arellano (GAA)
+ * Fecha:       Martes 4 de Mayo de 2021 (17_50)
+ *
+ * Historia:    .
+ *              20210504_1950 Creación de éste controlador REST
+ *
+ */
 package io.kebblar.petstore.api.service;
 
 import io.kebblar.petstore.api.model.domain.BlockCyperChecker;
