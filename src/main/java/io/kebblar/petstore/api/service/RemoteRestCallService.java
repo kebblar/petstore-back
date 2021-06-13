55e83d93a302daf511eba0f4a51b19ae0a72fb3a
Gustavo Adolfo Arellano Sandoval <arellano.gustavo@gmail.com>
2021-06-13 16:59:13 -0500

    Update RemoteRestCallService.java

--- src/main/java/io/kebblar/petstore/api/service/RemoteRestCallService.java
+++ src/main/java/io/kebblar/petstore/api/service/RemoteRestCallService.java
@@ -32,7 +32,7 @@ public interface RemoteRestCallService {
     /**
      * Recupera la información acerca de los movimientos de determinada cuenta dentro de
      * la blockchain de bitcoin.
-
+     *
      * @return Objeto que contiene la información requerida
      */
     BlockCyperChecker verifyBalance(String address);
@@ -41,7 +41,7 @@ public interface RemoteRestCallService {
      * Dado un monto en pesos, este método devuelve la cantidad convertida en bitcoin,
      * además de regresar el cambio que tomó el bitcoin para realizar la conversión llamando
      * a la api de bitso para recuperar los datos.
-
+     *
      * @return Objeto con la información del cambio y el monto el btc.
      */
     String convierte();
