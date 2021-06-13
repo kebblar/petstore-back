d2a3e444044812929ec3797fb22da902cbb51214
Gustavo Adolfo Arellano Sandoval <arellano.gustavo@gmail.com>
2021-06-13 13:05:43 -0500

    Update AnuncioService.java

--- src/main/java/io/kebblar/petstore/api/service/AnuncioService.java
+++ src/main/java/io/kebblar/petstore/api/service/AnuncioService.java
@@ -4,6 +4,5 @@ import io.kebblar.petstore.api.model.exceptions.BusinessException;
 import io.kebblar.petstore.api.model.response.DetalleAnuncioResponse;
 
 public interface AnuncioService {
-
     DetalleAnuncioResponse detalleAnuncio(int id) throws BusinessException;
 }
