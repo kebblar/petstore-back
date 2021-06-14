e212097c1826c8cd0d67f4ba7db8ffce9719fe5d
Gustavo Adolfo Arellano Sandoval <arellano.gustavo@gmail.com>
2021-06-14 12:16:21 -0500

    Update MascotaValorAtributoRequest.java

--- src/main/java/io/kebblar/petstore/api/model/request/MascotaValorAtributoRequest.java
+++ src/main/java/io/kebblar/petstore/api/model/request/MascotaValorAtributoRequest.java
@@ -7,7 +7,7 @@
  *              de: "AUTOR".
  *
  *              ------------------------------------------------
- * Artefacto:   ActualizaAnuncioRequest.java
+ * Artefacto:   MascotaValorAtributoRequest.java
  * Tipo:        clase
  * AUTOR:       Isabel Contreras (IS)
  * Fecha:       Jueves 6 de Mayo de 2021 (10_40)
@@ -28,22 +28,26 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * @since   1.0-SNAPSHOT
  */
 public class MascotaValorAtributoRequest {
-    
     private int id;
     @JsonProperty("valor")
     private int idValorAtributo;
+    
     public int getId() {
         return id;
     }
+    
     public void setId(int id) {
         this.id = id;
     }
+    
     public int getIdValorAtributo() {
         return idValorAtributo;
     }
+    
     public void setIdValorAtributo(int idValorAtributo) {
         this.idValorAtributo = idValorAtributo;
     }
+    
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -52,6 +56,7 @@ public class MascotaValorAtributoRequest {
         result = prime * result + idValorAtributo;
         return result;
     }
+    
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -67,19 +72,18 @@ public class MascotaValorAtributoRequest {
             return false;
         return true;
     }
+    
     @Override
     public String toString() {
         return "MascotaValorAtributoRequest [id=" + id + ", idValorAtributo=" + idValorAtributo + "]";
     }
+    
     public MascotaValorAtributoRequest(int id, int idValorAtributo) {
-        super();
         this.id = id;
         this.idValorAtributo = idValorAtributo;
     }
+    
     public MascotaValorAtributoRequest() {
-        super();
-        // TODO Auto-generated constructor stub
     }
-    
-    
+      
 }
