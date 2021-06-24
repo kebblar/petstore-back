43289d15f947fac302f25a014df1dc8540a0d09f
garellano <garellano@localhost>
2021-06-24 11:52:58 -0500

    bugfix change isBlank to trim().length()<1

--- src/main/java/io/kebblar/petstore/api/service/MunicipioServiceImpl.java
+++ src/main/java/io/kebblar/petstore/api/service/MunicipioServiceImpl.java
@@ -168,7 +168,7 @@ public class MunicipioServiceImpl implements MunicipioService {
     @Override
     public List<Municipio> getMunicipiosByPaisDescripcion(int pais, String nombre) throws BusinessException {
         try {
-        	if(pais > 0 && (nombre == null || nombre.isBlank())) {
+        	if(pais > 0 && (nombre == null || nombre.trim().length()<1 )) {
             return municipioMapper.getMunicipiosByPaisDescripcion(pais);
         	} else if (pais > 0 && nombre.length()>0 ) {
         	return municipioMapper.getMunicipiosByPaisNombreDescripcion(pais, nombre);
