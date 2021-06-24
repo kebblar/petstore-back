c6ba6bf2de377ff56d6bba9482692ab816d1ccbb
LuisAMUltra <langelm@ultrasist.com.mx>
2021-06-24 18:01:38 -0500

    Servicios para el catalogos de rol

---
+++ src/main/java/io/kebblar/petstore/api/rest/RolController.java
@@ -0,0 +1,158 @@
+
+/*
+ * Licencia:    Usted  puede  utilizar  libremente  este  código
+ *              para copiarlo,  distribuirlo o modificarlo total
+ *              o  parcialmente siempre y cuando  mantenga  este
+ *              aviso y  reconozca la  autoría del  código al no
+ *              modificar  los datos establecidos en  la mencion 
+ *              de "AUTOR".
+ *
+ *              ------------------------------------------------
+ * 
+ * Artefacto:   RolController .java
+ * Proyecto:    petstore
+ * Tipo:        clase 
+ * AUTOR:       Fhernanda Romo
+ * Fecha:       jueves 06 de junio de 2021 (17_26)
+ * 
+ *              ------------------------------------------------
+ *
+ * Historia:    20210624_1726 Implementación de clase 
+ *
+ */
+
+package io.kebblar.petstore.api.rest;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.kebblar.petstore.api.model.domain.Rol;
+import io.kebblar.petstore.api.service.RolService;
+import io.kebblar.petstore.api.model.exceptions.BusinessException;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * <p>Descripción:</p>
+ * Implementacion  del REST Controller asociado a los endpoints de  gestión del POJO 'rol'. 
+ *
+ * <p>Todos los métodos de esta clase disparan {@link BusinessException}
+ * 
+ * <p>NOTA IMPORTANTE: Los distntos métodos de este controlador no llevan javadoc debido 
+ *                     a que la  documentación Swagger API cumple con ese objetivo.
+ *
+ * @author Fhernanda Romo
+ * @version 1.0-SNAPSHOT
+ * @since 1.0-SNAPSHOT
+ *
+ * @see io.kebblar.petstore.api.model.domain.Rol
+ * @see io.kebblar.petstore.api.service.RolService
+ */
+
+@RestController
+@Api(value = "administracion")
+@RequestMapping(value = "/api")
+public class RolController {
+
+    private RolService rolService;
+
+    /**
+     * Constructor que realiza el setting de los servicios que serán 
+     * utilizados en este controlador.
+     * 
+     * @param rolService Servicios de usuario
+     */
+    public RolController(RolService rolService) {
+        this.rolService = rolService;
+    }
+
+    @ApiOperation(
+        value = "RolController::getAll",
+        notes = "Regresa una lista de todos los objetos Rol "
+            + "debidamente paginados con base en el payload de "
+            + "request que determina el tamaño de la página, la "
+            + "longitud de la página, el campo por el que se va a "
+            + "ordenar y si el orden es ascendente o descendente."
+            + "<br/><br/>"
+            + "En el caso de que los parámetros proporcionados "
+            + "<b><i><label style='color:blue;'>excedan</label><i></b> las "
+            + "dimensiones de la lista real de datos, este método es "
+            + "capaz de ajustar lo necesario para que la lista resultante "
+            + "sea suceptible de ser manipulada adecuadamente.")
+    @GetMapping(
+        value = "/roles.json",
+        produces = "application/json; charset=utf-8")
+    public List<Rol> getAllRol() throws BusinessException {
+        return rolService.getAll();
+    }
+    
+    @ApiOperation(
+        value = "RolController::get",
+        notes = "Regresa un objeto Rol cuyo id "
+            + "coincide con el entero recibido como parametro.")
+    @GetMapping(
+        value = "/rol/{id}.json",
+        produces = "application/json; charset=utf-8")
+    public Rol getRol(
+    @ApiParam(name="id", value="Representa el id del rol buscado.")
+    @PathVariable int id
+    ) throws BusinessException {
+        return this.rolService.getById(id);
+    }
+    
+    @ApiOperation(
+        value = "RolController::insert",
+        notes = "Recibe un objeto Rol el cual debe de ser insertado "
+            + " como dato dentro de la base de datos del sistema.")
+    @PostMapping(
+            value = "/rol.json",
+            produces = "application/json; charset=utf-8")
+    public int insert(
+    @ApiParam(name="rol", value="Rol que será insertado en el sistema.")
+    @RequestBody Rol rol
+    ) throws BusinessException {
+        return rolService.insert(rol);
+    }
+
+    @ApiOperation(
+        value = "RolController::update",
+        notes = "Recibe un objeto Rol, este objeto es buscado por "
+            + "id dentro de la base de datos y es actualizado con el resto de "
+            + "datos proporcionados si es que el id en efecto existe. ")
+    @PutMapping(
+            value = "/rol.json",
+            produces = "application/json; charset=utf-8")
+    public int update(
+    @ApiParam(name="rol", value="Rol que será actualizado en el sistema, el id debe coincidir con el id del objeto que se desea actualizar.")
+    @RequestBody Rol rol
+    ) throws BusinessException {
+        return rolService.update(rol);
+    }
+    
+    @ApiOperation(
+        value = "RolController::delete",
+        notes = "Recibe un objeto Rol, el cual es buscado dentro de "
+        +"la base de datos y en caso de existir es eliminado.")
+    @DeleteMapping(
+            value = "/rol.json",
+            produces = "application/json; charset=utf-8")
+    public int delete(
+    @ApiParam(name="rol", value="Rol que será removido del sistema.")
+    @RequestBody Rol rol
+    ) throws BusinessException {
+        return rolService.delete(rol);
+    }
+    
+}
---
+++ src/main/java/io/kebblar/petstore/api/service/RolServiceImpl.java
@@ -0,0 +1,150 @@
+
+/*
+ * Licencia:    Usted  puede  utilizar  libremente  este  código
+ *              para copiarlo,  distribuirlo o modificarlo total
+ *              o  parcialmente siempre y cuando  mantenga  este
+ *              aviso y  reconozca la  autoría del  código al no
+ *              modificar  los datos establecidos en  la mencion 
+ *              de "AUTOR".
+ *
+ *              ------------------------------------------------
+ * 
+ * Artefacto:   RolServiceImpl .java
+ * Proyecto:    petstore
+ * Tipo:        clase 
+ * AUTOR:       Fhernanda Romo
+ * Fecha:       jueves 06 de junio de 2021 (17_26)
+ * 
+ *              ------------------------------------------------
+ *
+ * Historia:    20210624_1726 Implementación de clase 
+ *
+ */
+
+package io.kebblar.petstore.api.service;
+
+import java.util.List;
+import java.sql.SQLException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import io.kebblar.petstore.api.model.domain.Rol;
+import io.kebblar.petstore.api.mapper.RolMapper;
+import io.kebblar.petstore.api.model.exceptions.BusinessException;
+
+/**
+ * <p>Descripción:</p>
+ * Servicio asociado a la entidad 'rol'. 
+ *
+ * <p>Implementación de la interfaz {@link RolService}.
+ * 
+ * <p>Todos los métodos de esta clase disparan {@link BusinessException}
+ *
+ * @author Fhernanda Romo
+ * @version 1.0-SNAPSHOT
+ * @since 1.0-SNAPSHOT
+ *
+ * @see  io.kebblar.petstore.api.model.domain.Rol
+ * @see  io.kebblar.petstore.api.service.RolService
+ */
+
+@Service("rolService")
+public class RolServiceImpl implements RolService {
+
+    private static final Logger logger = LoggerFactory.getLogger(RolServiceImpl.class);
+
+    private RolMapper rolMapper;
+
+    /**
+     * Constructor que realiza el setting de todos los Mappers y todos los
+     * servicios adicionales a ser empleados en esta clase.
+     * 
+     * @param rolMapper mapper utilizado para llamar a metodos de persistencia
+     */
+    public RolServiceImpl(RolMapper rolMapper) {
+        this.rolMapper = rolMapper;
+    }
+
+    /*
+    * Implementación del método getById
+    */
+    @Override
+    public Rol getById(int id) throws BusinessException {
+        try {
+            return rolMapper.getById(id);
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+            throw new BusinessException();
+        }
+    }
+
+    /*
+    * Implementación del método getAll
+    */
+    @Override
+    public List<Rol> getAll() throws BusinessException {
+        try {
+            return rolMapper.getAllSinFiltros();
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+            throw new BusinessException();
+        }
+    }
+
+    /*
+    * Implementación del método insert
+    */
+    @Override
+    public int insert(Rol rol) throws BusinessException {
+        try {
+            return rolMapper.insert(rol);
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+            throw new BusinessException();
+        }
+    }
+
+    /*
+    * Implementación del método update
+    */
+    @Override
+    public int update(Rol rol) throws BusinessException {
+        try {
+            return rolMapper.update(rol);
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+            throw new BusinessException();
+        }
+    }
+
+    /*
+    * Implementación del método delete
+    */
+    @Override
+    public int delete(Rol rol) throws BusinessException {
+        try {
+            return rolMapper.delete(rol.getId());
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+            throw new BusinessException();
+        }
+    }
+
+    /*
+    * Implementación del método save
+    */
+    @Override
+    public int save(Rol rol) throws BusinessException {
+        try {
+            if (getById(rol.getId()) == null) {
+                return rolMapper.insert(rol);
+            } else {
+                return rolMapper.update(rol);
+            }
+        } catch (SQLException e) {
+            logger.error(e.getMessage());
+            throw new BusinessException();
+        }
+    }
+
+}
