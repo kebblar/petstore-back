3c90980ca96950c44c79188ba28aa83458dd0f6f
Gustavo Adolfo Arellano Sandoval <arellano.gustavo@gmail.com>
2021-06-27 19:21:56 -0500

    Update AnuncioServiceImpl.java

--- src/main/java/io/kebblar/petstore/api/service/AnuncioServiceImpl.java
+++ src/main/java/io/kebblar/petstore/api/service/AnuncioServiceImpl.java
@@ -306,11 +306,10 @@ public class AnuncioServiceImpl implements AnuncioService {
                 throw new BusinessException("Error de datos","No se encontro informacion",4091,"CVE_4091",HttpStatus.CONFLICT);
             }
             short status = detalleResponse.getIdEstatus();
-            if( AnuncioEstatusEnum.ACTIVO.getId()!=status &&
-                AnuncioEstatusEnum.PUBLICADO.getId()!=status) {
+            if( AnuncioEstatusEnum.ELIMINADO.getId()==status) {
                 logger.error("Este anuncio (folio: "+detalleResponse.getFolio()+") NO se encuentra ni ACTIVO ni PUBLICADO id: " + id);
-                //throw new BusinessException("Error de datos","Anuncio no disponible",4091,"CVE_4091",HttpStatus.CONFLICT);
-                return null;
+                throw new BusinessException("Error de datos","Anuncio no disponible",4091,"CVE_4091",HttpStatus.CONFLICT);
+                //return null;
             }
             //Se consulta la informacion de los atributos del anuncio
             List<MascotaValorAtributoResponse> atributosResponse = anuncioMapper.valorAtributosPorAnuncio(id);
