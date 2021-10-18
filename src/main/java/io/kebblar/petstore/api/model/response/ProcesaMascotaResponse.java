11ab316fe063e9679512b40417814ca86f9dd425
fherevans <fher@ciencias.unam.mx>
2021-10-18 14:06:43 -0500

    Quitando mascota y mediatipo

--- src/main/java/io/kebblar/petstore/api/model/response/ProcesaMascotaResponse.java
+++
@@ -1,6 +0,0 @@
-package io.kebblar.petstore.api.model.response;
-
-public class ProcesaMascotaResponse {
-    public ProcesaMascotaResponse(String la_mascota_fué_insertada_correctamente, int id) {
-    }
-}
