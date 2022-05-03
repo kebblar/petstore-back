be980d51c65ee31141f8c52e1516c82c44881834
garellano <garellano@192.168.100.99>
2022-05-03 09:15:10 -0500

    se modifica la forma en la que se gestiona la fecha

--- src/test/java/io/kebblar/petstore/api/utils/AnuncioUtilTest.java
+++ src/test/java/io/kebblar/petstore/api/utils/AnuncioUtilTest.java
@@ -2,6 +2,8 @@ package io.kebblar.petstore.api.utils;
 
 import org.junit.Test;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
 
 public class AnuncioUtilTest {
@@ -10,23 +12,29 @@ public class AnuncioUtilTest {
     public void generaFolioTest () {
         assert(AnuncioUtil.generaFolio().matches("[0-9]{14}"));
     }
+    
+    private Date calc(LocalDate valor) {
+        ZoneId systemTimeZone = ZoneId.systemDefault();
+        ZonedDateTime zonedDateTime = valor.atStartOfDay(systemTimeZone);
+        return Date.from(zonedDateTime.toInstant());
+    }
 
     @Test
     public void validaFechasTest() {
-        Date inicio = java.sql.Date.valueOf(LocalDate.now().plusMonths(2));
-        Date fin = java.sql.Date.valueOf(LocalDate.now().plusMonths(3));
+        Date inicio = calc(LocalDate.now().plusMonths(2));
+        Date fin = calc(LocalDate.now().plusMonths(3));
         assert(AnuncioUtil.validaFechasPeriodo(inicio,fin));
         assert(!AnuncioUtil.validaFechasPeriodo(fin,inicio));
         assert(AnuncioUtil.validaFechasPeriodo(null, null));
-        inicio = java.sql.Date.valueOf(LocalDate.now().minusMonths(3));
-        fin = java.sql.Date.valueOf(LocalDate.now().minusMonths(2));
+        inicio = calc(LocalDate.now().minusMonths(3));
+        fin = calc(LocalDate.now().minusMonths(2));
         assert(!AnuncioUtil.validaFechasPeriodo(inicio,fin));
     }
 
     @Test
     public void comparaFechasTest() {
-        Date inicio = java.sql.Date.valueOf(LocalDate.now());
-        Date fin = java.sql.Date.valueOf(LocalDate.now().plusMonths(1));
+        Date inicio = calc(LocalDate.now());
+        Date fin = calc(LocalDate.now().plusMonths(1));
         assert (AnuncioUtil.comparafechas(inicio,fin)!=0);
         inicio = fin;
         assert (AnuncioUtil.comparafechas(inicio,fin)==0);
